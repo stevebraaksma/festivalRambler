02caf16e8b84a8d4a3a5e87a4075fb89d21875de
Steve Braaksma <stevebraaksma@hotmail.com>
2021-09-20 22:43:00 -0500

    api data displayed now

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ const API_KEY = 'df0b827a-3ec6-494d-b53e-b6d72191cc6f';
 let festivalData;
 
 
-
 // startDate=2017-01-01&
 
     // https://edmtrain.com/api/events?festivalInd=true&client=df0b827a-3ec6-494d-b53e-b6d72191cc6f
 
+const $outputFestName = $('#output-fest');
+// need to add the other outputs here, but first test
 
-    
     const $form = $('form');
     
     const $inputDriveStart = $('input[id="drive-start"]');
@@ -53,46 +53,50 @@ let festivalData;
             festivalData = data;
             console.log(festivalData);
             // render();
-    
+            console.log(festivalData.data.name);
+
+
+            console.log(festivalData.data[0].name);
+
+            console.log(festivalData['data'][20]);
+            // the above returns the full event array
+            console.log(festivalData['data'][20]['name']);
+
+            let mrKey = Object.entries(festivalData['data']);
+            console.log(mrKey.length);
+            // correctly returns 153 as the length, yay!!!
+            let mrCounter = 5;
+            console.log(mrKey);
+            console.log(mrKey[5]);
+            console.log(mrKey[5][1]);
+            console.log(mrKey[5][1]['name']);
+            console.log(mrKey[mrCounter][1]['name']);
+
+
+            
+            mrKey.forEach(function() {
+                let thisIsTheOne = mrKey.pop();
+                console.log(thisIsTheOne);
+                let currentFestBeingAddedNow =
+                $(`<tr>
+                <td>${thisIsTheOne[1]['name']}</td>
+                <td>${thisIsTheOne[1]['venue']['location']}</td>
+                </tr>`)
+                $('tbody').append(currentFestBeingAddedNow);
+            })
+
         }, function(error) {
         console.log(error);
         })
     }
 
+    function render() {
+        $outputFestName.text(`${festivalData.data[0].name}`);
+        $outputFestName.text(festivalData.data[0].name);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        console.log($outputFestName);
+    }
 
 
 });
