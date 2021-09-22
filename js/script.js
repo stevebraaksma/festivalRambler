2aa5e732ac5c05d8fa83303c4ac66faac79f4405
Steve Braaksma <stevebraaksma@hotmail.com>
2021-09-21 20:13:12 -0500

    pivoted to region

--- js/script.js
+++ js/script.js
@@ -2,101 +2,163 @@
 $(function(){
 
 
-const BASE_URL = 'https://edmtrain.com';
-const API_KEY = 'df0b827a-3ec6-494d-b53e-b6d72191cc6f';
+//edmTrain
+const EDM_TRAIN_BASE_URL = 'https://edmtrain.com';
+const EDM_TRAIN_API_KEY = 'df0b827a-3ec6-494d-b53e-b6d72191cc6f';
 let festivalData;
 
-
 // startDate=2017-01-01&
-
     // https://edmtrain.com/api/events?festivalInd=true&client=df0b827a-3ec6-494d-b53e-b6d72191cc6f
+
+ 
 
 const $outputFestName = $('#output-fest');
 // need to add the other outputs here, but first test
 
-    const $form = $('form');
+const $form = $('form');
     
-    const $inputDriveStart = $('input[id="drive-start"]');
+const $inputDriveStart = $('input[id="drive-start"]');
 
-    const $inputMaxDrive = $('select[id="max-drive"]');
+const $inputRegion = $('select[id="region"]');
     //  the above could also be written as:
-    // const $inputMaxDrive = $("#max-drive");
+    // const $inputRegion = $("#region");
     // may want to clean up everything to that shorter form
 
-    const $inputStartDate = $('input[id="start-date"]');
-    const $inputEndDate = $('input[id="end-date"]');
+const $inputStartDate = $('input[id="start-date"]');
+const $inputEndDate = $('input[id="end-date"]');
  
+    
+$form.on('submit', handleGetData);
 
     
-    $form.on('submit', handleGetData);
+function handleGetData(event) {
+    event.preventDefault();    
+
+    // const driveStart = $inputDriveStart.val();
+    // console.log(driveStart);
+
+    const region = $inputRegion.val();
+    console.log(region);
+
+    // const startDate = $inputStartDate.val();
+    // console.log(startDate);
+
+    // const endDate = $inputEndDate.val();
+    // console.log(endDate);
+
+
+    const midwestArray = ['Iowa', 'Illinois', 'Indiana', 'Kansas', 'Michigan', 'Minnesota', 'Missouri', 
+    'North Dakota', 'Nebraska', 'OhioSouth Dakota', 'Wisconsin'];
     
-    
-    function handleGetData(event) {
-        event.preventDefault();    
 
-        const driveStart = $inputDriveStart.val();
-        console.log(driveStart);
-
-        const maxDrive = $inputMaxDrive.val();
-        console.log(maxDrive);
-
-        const startDate = $inputStartDate.val();
-        console.log(startDate);
-
-        const endDate = $inputEndDate.val();
-        console.log(endDate);
+    const northeastArray = ['Connecticut', 'Massachusetts', 'Maine', 'New Hampshire', 'New Jersey', 'New York', 'Pennsylvania', 'Rhode Island', 'Vermont'];
 
 
 
-        $.ajax(`https://edmtrain.com/api/events?festivalInd=true&endDate=2021-11-25&client=df0b827a-3ec6-494d-b53e-b6d72191cc6f`).then(function(data) {
-            console.log(data);
-            festivalData = data;
-            console.log(festivalData);
+
+    const southArray = ['Alabama', 'Arkansas', 'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Kentucky', 'Louisiana', 'Maryland', 'Mississippi', 'North Carolina', 'Oklahoma', 'South Carolina', 'Tennessee', 'Texas', 'Virginia', 'West Virginia'];
+
+
+
+    const westArray = ['Alaska', 'Arizona', 'California', 'Colorado', 'Hawaii', 'Idaho', 'Montana', 'New Mexico', 'Nevada', 'Oregon', 'Utah', 'Washington', 'Wyoming'];
+
+
+
+    const allUSArray = ['Iowa', 'Illinois', 'Indiana', 'Kansas', 'Michigan', 'Minnesota', 'Missouri', 
+    'North Dakota', 'Nebraska', 'OhioSouth Dakota', 'Wisconsin', 'Connecticut', 'Massachusetts', 'Maine', 
+    'New Hampshire', 'New Jersey', 'New York', 'Pennsylvania', 'Rhode Island', 'Vermont', 'Alabama', 'Arkansas', 
+    'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Kentucky', 'Louisiana', 'Maryland', 'Mississippi', 
+    'North Carolina', 'Oklahoma', 'South Carolina', 'Tennessee', 'Texas', 'Virginia', 'West Virginia', 'Alaska', 
+    'Arizona', 'California', 'Colorado', 'Hawaii', 'Idaho', 'Montana', 'New Mexico', 'Nevada', 'Oregon', 
+    'Utah', 'Washington', 'Wyoming'];
+
+
+
+
+
+    //   &endDate=2021-11-25   removed
+
+
+    $.ajax(`https://edmtrain.com/api/events?festivalInd=true&client=df0b827a-3ec6-494d-b53e-b6d72191cc6f`).then(function(data) {
+        console.log(data);
+        festivalData = data;
             // render();
-            console.log(festivalData.data.name);
 
+        let mrKey = Object.entries(festivalData['data']);
 
-            console.log(festivalData.data[0].name);
+        mrKey.forEach(function() {
+            let thisIsTheOne = mrKey.shift();
 
-            console.log(festivalData['data'][20]);
-            // the above returns the full event array
-            console.log(festivalData['data'][20]['name']);
-
-            let mrKey = Object.entries(festivalData['data']);
-            console.log(mrKey.length);
-            // correctly returns 153 as the length, yay!!!
-            let mrCounter = 5;
-            console.log(mrKey);
-            console.log(mrKey[5]);
-            console.log(mrKey[5][1]);
-            console.log(mrKey[5][1]['name']);
-            console.log(mrKey[mrCounter][1]['name']);
-
-
+            let stateState = thisIsTheOne[1]['venue']['state'];
             
-            mrKey.forEach(function() {
-                let thisIsTheOne = mrKey.pop();
-                console.log(thisIsTheOne);
-                let currentFestBeingAddedNow =
-                $(`<tr>
-                <td>${thisIsTheOne[1]['name']}</td>
-                <td>${thisIsTheOne[1]['venue']['location']}</td>
-                </tr>`)
-                $('tbody').append(currentFestBeingAddedNow);
-            })
+            if (region === 'Midwest') {
+                if (midwestArray.includes(stateState)) {
+                    let currentFestBeingAddedNow =
+                    $(`<tr>
+                    <td>${thisIsTheOne[1]['name']}</td>
+                    <td>${thisIsTheOne[1]['venue']['location']}</td>
+                    <td>${thisIsTheOne[1]['date']}</td>
+                    </tr>`)
+                    $('tbody').append(currentFestBeingAddedNow);
+                }
+            }
+            
+            if (region === 'Northeast') {
+                if (northeastArray.includes(stateState)) {
+                    let currentFestBeingAddedNow =
+                    $(`<tr>
+                    <td>${thisIsTheOne[1]['name']}</td>
+                    <td>${thisIsTheOne[1]['venue']['location']}</td>
+                    <td>${thisIsTheOne[1]['date']}</td>
+                    </tr>`)
+                    $('tbody').append(currentFestBeingAddedNow);
+                }
+            }
+
+            if (region === 'South') {
+                if (southArray.includes(stateState)) {
+                    let currentFestBeingAddedNow =
+                    $(`<tr>
+                    <td>${thisIsTheOne[1]['name']}</td>
+                    <td>${thisIsTheOne[1]['venue']['location']}</td>
+                    <td>${thisIsTheOne[1]['date']}</td>
+                    </tr>`)
+                    $('tbody').append(currentFestBeingAddedNow);
+                }
+            }    
+
+            if (region === 'West') {
+                if (westArray.includes(stateState)) {
+                    let currentFestBeingAddedNow =
+                    $(`<tr>
+                    <td>${thisIsTheOne[1]['name']}</td>
+                    <td>${thisIsTheOne[1]['venue']['location']}</td>
+                    <td>${thisIsTheOne[1]['date']}</td>
+                    </tr>`)
+                    $('tbody').append(currentFestBeingAddedNow);
+                }
+            } 
+
+            if (region === 'all US') {
+                if (allUSArray.includes(stateState)) {
+                    let currentFestBeingAddedNow =
+                    $(`<tr>
+                    <td>${thisIsTheOne[1]['name']}</td>
+                    <td>${thisIsTheOne[1]['venue']['location']}</td>
+                    <td>${thisIsTheOne[1]['date']}</td>
+                    </tr>`)
+                    $('tbody').append(currentFestBeingAddedNow);
+                }
+            }             
+
+
+        })
 
         }, function(error) {
         console.log(error);
         })
     }
 
-    function render() {
-        $outputFestName.text(`${festivalData.data[0].name}`);
-        $outputFestName.text(festivalData.data[0].name);
-
-
-        console.log($outputFestName);
-    }
 
 
 });
