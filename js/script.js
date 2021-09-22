ddb116a5875311f1a936c9684c2322965178d941
Steve Braaksma <stevebraaksma@hotmail.com>
2021-09-22 13:59:54 -0500

    additional code cleanup

--- js/script.js
+++ js/script.js
@@ -3,118 +3,70 @@
 $(function(){
 
 
-//edmTrain
+//edmtrain API
 const EDM_TRAIN_BASE_URL = 'https://edmtrain.com';
 const EDM_TRAIN_API_KEY = 'df0b827a-3ec6-494d-b53e-b6d72191cc6f';
-let festivalData;
 
-// startDate=2017-01-01&
-    // https://edmtrain.com/api/events?festivalInd=true&client=df0b827a-3ec6-494d-b53e-b6d72191cc6f
-
- 
-
-// const $outputFestName = $('#output-fest');
-// need to add the other outputs here, but first test
+// regions
+const midwestArray = ['Iowa', 'Illinois', 'Indiana', 'Kansas', 'Michigan', 'Minnesota', 'Missouri', 'North Dakota', 'Nebraska', 'Ohio', 'South Dakota', 'Wisconsin'];
+const northeastArray = ['Connecticut', 'Massachusetts', 'Maine', 'New Hampshire', 'New Jersey', 'New York', 'Pennsylvania', 'Rhode Island', 'Vermont'];
+const southArray = ['Alabama', 'Arkansas', 'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Kentucky', 'Louisiana', 'Maryland', 'Mississippi', 'North Carolina', 'Oklahoma', 'South Carolina', 'Tennessee', 'Texas', 'Virginia', 'West Virginia'];
+const westArray = ['Alaska', 'Arizona', 'California', 'Colorado', 'Hawaii', 'Idaho', 'Montana', 'New Mexico', 'Nevada', 'Oregon', 'Utah', 'Washington', 'Wyoming'];
+const allUSArray = ['Iowa', 'Illinois', 'Indiana', 'Kansas', 'Michigan', 'Minnesota', 'Missouri', 
+'North Dakota', 'Nebraska', 'Ohio', 'South Dakota', 'Wisconsin', 'Connecticut', 'Massachusetts', 'Maine', 
+'New Hampshire', 'New Jersey', 'New York', 'Pennsylvania', 'Rhode Island', 'Vermont', 'Alabama', 'Arkansas', 
+'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Kentucky', 'Louisiana', 'Maryland', 'Mississippi', 
+'North Carolina', 'Oklahoma', 'South Carolina', 'Tennessee', 'Texas', 'Virginia', 'West Virginia', 'Alaska', 
+'Arizona', 'California', 'Colorado', 'Hawaii', 'Idaho', 'Montana', 'New Mexico', 'Nevada', 'Oregon', 
+'Utah', 'Washington', 'Wyoming'];
 
 const $form = $('form');
-    
-// const $inputDriveStart = $('input[id="drive-start"]');
-
 const $inputRegion = $("#region");
-    //  the above could also be written as:
-    // const $inputRegion = $("#region");
-    // may want to clean up everything to that shorter form
 
+let festivalData;
 
- 
-    
 $form.on('submit', handleGetData);
 
-
-
-    
 function handleGetData(event) {
     event.preventDefault();    
-
-    // const driveStart = $inputDriveStart.val();
-    // console.log(driveStart);
-
     const region = $inputRegion.val();
 
-    // const startDate = $inputStartDate.val();
-    // console.log(startDate);
-
-    // const endDate = $inputEndDate.val();
-    // console.log(endDate);
-
-
-    const midwestArray = ['Iowa', 'Illinois', 'Indiana', 'Kansas', 'Michigan', 'Minnesota', 'Missouri', 
-    'North Dakota', 'Nebraska', 'Ohio', 'South Dakota', 'Wisconsin'];
-    
-
-    const northeastArray = ['Connecticut', 'Massachusetts', 'Maine', 'New Hampshire', 'New Jersey', 'New York', 'Pennsylvania', 'Rhode Island', 'Vermont'];
-
-
-
-
-    const southArray = ['Alabama', 'Arkansas', 'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Kentucky', 'Louisiana', 'Maryland', 'Mississippi', 'North Carolina', 'Oklahoma', 'South Carolina', 'Tennessee', 'Texas', 'Virginia', 'West Virginia'];
-
-
-
-    const westArray = ['Alaska', 'Arizona', 'California', 'Colorado', 'Hawaii', 'Idaho', 'Montana', 'New Mexico', 'Nevada', 'Oregon', 'Utah', 'Washington', 'Wyoming'];
-
-
-
-    const allUSArray = ['Iowa', 'Illinois', 'Indiana', 'Kansas', 'Michigan', 'Minnesota', 'Missouri', 
-    'North Dakota', 'Nebraska', 'Ohio', 'South Dakota', 'Wisconsin', 'Connecticut', 'Massachusetts', 'Maine', 
-    'New Hampshire', 'New Jersey', 'New York', 'Pennsylvania', 'Rhode Island', 'Vermont', 'Alabama', 'Arkansas', 
-    'District of Columbia', 'Delaware', 'Florida', 'Georgia', 'Kentucky', 'Louisiana', 'Maryland', 'Mississippi', 
-    'North Carolina', 'Oklahoma', 'South Carolina', 'Tennessee', 'Texas', 'Virginia', 'West Virginia', 'Alaska', 
-    'Arizona', 'California', 'Colorado', 'Hawaii', 'Idaho', 'Montana', 'New Mexico', 'Nevada', 'Oregon', 
-    'Utah', 'Washington', 'Wyoming'];
-
-
-    $.ajax(`https://edmtrain.com/api/events?festivalInd=true&client=df0b827a-3ec6-494d-b53e-b6d72191cc6f`).then(function(data) {
+    $.ajax(`${EDM_TRAIN_BASE_URL}/api/events?festivalInd=true&client=${EDM_TRAIN_API_KEY}`).then(function(data) {
         festivalData = data;
-
-        let mrKey = Object.entries(festivalData['data']);
-
-        mrKey.forEach(function() {
-            let thisIsTheOne = mrKey.shift();
-            let stateState = thisIsTheOne[1]['venue']['state'];
-            
+        // transform using .entries to more easily access data
+        let festDataTransformed = Object.entries(festivalData['data']);
+        // iterate through the data with forEach
+        festDataTransformed.forEach(function() {
+            let currentFest = festDataTransformed.shift();
+            let currrentState = currentFest[1]['venue']['state'];
+            // append to page if conditions are met
             if (region === 'Midwest') {
-                if (midwestArray.includes(stateState)) {
+                if (midwestArray.includes(currrentState)) {
                     appendEvent();
                 }
             }
-
             if (region === 'Northeast') {
-                if (northeastArray.includes(stateState)) {
+                if (northeastArray.includes(currrentState)) {
                     appendEvent();
                 }
             }
-
             if (region === 'South') {
-                if (southArray.includes(stateState)) {
+                if (southArray.includes(currrentState)) {
                     appendEvent();
                 }
             }    
-
             if (region === 'West') {
-                if (westArray.includes(stateState)) {
+                if (westArray.includes(currrentState)) {
                     appendEvent();
                 }
             } 
-
             if (region === 'all US') {
-                if (allUSArray.includes(stateState)) {
+                if (allUSArray.includes(currrentState)) {
                     appendEvent();
                 }
             }      
-
             if (region === 'other') {
-                if (allUSArray.includes(stateState) === false && thisIsTheOne[1]['venue']['location'] !== 'Virtual')  {
+                if (allUSArray.includes(currrentState) === false && currentFest[1]['venue']['location'] !== 'Virtual')  {
                     appendEvent();
                 } 
             } 
@@ -122,10 +74,10 @@ function handleGetData(event) {
             function appendEvent(){
                 let currentFestBeingAddedNow =
                 $(`<tr>
-                <td>${thisIsTheOne[1]['name']}</td>
-                <td>${thisIsTheOne[1]['venue']['location']}</td>
-                <td>${thisIsTheOne[1]['date']}</td>
-                <td>  <a href=${thisIsTheOne[1]['link']} target="_blank"> <i class="fas fa-external-link-alt"></i></a></td>
+                <td>${currentFest[1]['name']}</td>
+                <td>${currentFest[1]['venue']['location']}</td>
+                <td>${currentFest[1]['date']}</td>
+                <td>  <a href=${currentFest[1]['link']} target="_blank"> <i class="fas fa-external-link-alt"></i></a></td>
                 </tr>`)
                 $('tbody').append(currentFestBeingAddedNow);
             }
@@ -141,5 +93,4 @@ function handleGetData(event) {
         $("table").find("tr:gt(0)").remove();
     });
      
-
 });
